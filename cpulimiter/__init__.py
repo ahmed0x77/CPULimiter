9ce3fb8540a3e0c911a4195050e26bfb90245064
ahmed0x77 <77070694+ahmed0x77@users.noreply.github.com>
2025-07-05 10:28:33 +0200

    Sync version in __init__.py to match setup.py and pyproject.toml

--- cpulimiter/__init__.py
+++ cpulimiter/__init__.py
@@ -2,7 +2,7 @@
 cpulimiter - A simple, lightweight Python library for Windows to limit CPU usage of processes.
 """
 
-__version__ = "1.5.0"
+__version__ = "1.0.2"
 __author__ = "Ahmed Ashraf"
 
 from .limiter import CpuLimiter
