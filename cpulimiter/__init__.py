1f62512ee0dc810354ef5e63590a9fa053e45b6e
ahmed0x77 <77070694+ahmed0x77@users.noreply.github.com>
2025-07-05 10:31:11 +0200

    Manually bump version to 1.0.3 to fix workflow sync issue [skip-version]

--- cpulimiter/__init__.py
+++ cpulimiter/__init__.py
@@ -2,7 +2,7 @@
 cpulimiter - A simple, lightweight Python library for Windows to limit CPU usage of processes.
 """
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __author__ = "Ahmed Ashraf"
 
 from .limiter import CpuLimiter
