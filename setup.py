396ced43bd594d0c68406d1a98eb2b6bfb846cd5
GitHub Action <action@github.com>
2025-07-05 14:54:04 +0000

    🚀 Bump version to 1.0.9 [patch] [skip-version]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cpulimiter",
-    version="1.0.8",
+    version="1.0.9",
     author="Ahmed Ashraf",
     author_email="your-email@example.com",
     description="A simple, lightweight Python library for Windows to limit the CPU usage of any running process",
